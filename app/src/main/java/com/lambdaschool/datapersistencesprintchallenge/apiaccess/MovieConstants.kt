073bb393e1246cd29dd5d2e4e55ec79985f31e98
Matthew Nelson <05nelsonm@protonmail.com>
2019-09-20 12:08:56 -0400

    removed base_url from the endpoint variable that I created

--- app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/apiaccess/MovieConstants.kt
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/apiaccess/MovieConstants.kt
@@ -6,5 +6,5 @@ object MovieConstants {
     const val API_KEY_PARAM = "359211348348a13a2b996217f7538f45"
     const val FIXED_QUERY_PARAMS_1 = "&language=en-US&query="
     const val FIXED_QUERY_PARAMS_2 = "&page=1&include_adult=false"
-    const val SEARCH_ENDPOINT = "$BASE_URL$ENDPOINT$API_KEY_PARAM$FIXED_QUERY_PARAMS_1"
+    const val SEARCH_ENDPOINT = "$ENDPOINT$API_KEY_PARAM$FIXED_QUERY_PARAMS_1"
 }
